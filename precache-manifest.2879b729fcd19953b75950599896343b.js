66776682a0be058e2b97c96544c6fb4a3f2dcff6
JiYoung <qnfmtm666@gmail.com>
2020-02-02 17:57:09 +0900

    Updates

---
+++ precache-manifest.2879b729fcd19953b75950599896343b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bb290cb3d6e913a2be511522afafc7",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a3b9eff7791569745f57",
+    "url": "/movie-app/static/css/main.2dcf6866.chunk.css"
+  },
+  {
+    "revision": "e8d091b104baa148258e",
+    "url": "/movie-app/static/js/2.07786ad8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie-app/static/js/2.07786ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b9eff7791569745f57",
+    "url": "/movie-app/static/js/main.2e17d6d4.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
